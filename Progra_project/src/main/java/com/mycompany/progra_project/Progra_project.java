5115fdcbe67c20fa220c5cc750663a0d45a8b058
xiomara nuñez umaña <johnnyfranco73@gmail.com>
2025-10-02 13:03:18 -0600

    test_01

---
+++ Progra_project/src/main/java/com/mycompany/progra_project/Progra_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progra_project;
+
+/**
+ *
+ * @author xio_d
+ */
+public class Progra_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
