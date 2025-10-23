f7692105e116d060f4703c4e1fb0435f81f468f2
letyavyespinoza <letyavy.jo.espinozarivas@gmail.com>
2025-10-23 12:18:22 -0600

    Create class Invoice.java

---
+++ Progra_project/src/main/java/Domain/Invoice.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domain;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ *
+ * @author USER
+ */
+public class Invoice {
+    private String invoiceNumber;
+    private Ticket ticket;
+    private double price;
+
+    public Invoice(String invoiceNumber, Ticket ticket, double price) {
+        this.invoiceNumber = invoiceNumber;
+        this.ticket = ticket;
+        this.price = price;
+    }
+   public String getInvoiceNumber() {
+        return invoiceNumber; 
+    }
+   
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+   public Ticket getTicket() { 
+        return ticket; 
+    }
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+   public double getPrice() { 
+        return price; 
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String formattedPrice() {
+        NumberFormat fmt = NumberFormat.getCurrencyInstance(Locale.getDefault());
+        return fmt.format(price);
+    }
+
+    @Override
+    public String toString() {
+           return "Invoice#: " + invoiceNumber 
+                + "\nFlight: " + ticket.getFlight().getRoute()
+                + "\nPassenger: " + ticket.getPassenger().getFullName()
+                + "\nID Number: " + ticket.getPassenger().getIdNumber()
+                +"\nEmail: " + ticket.getPassenger().getEmail()
+                +"\nPhone Number: " + ticket.getPassenger().getPhoneNumber()   
+                + "\nPrice: " + formattedPrice();
+    }
+}
