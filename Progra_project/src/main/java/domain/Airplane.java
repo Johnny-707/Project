b955a49b0a20473dc0a8385be75fb2cedb903f3d
Zulay71 <zulaybustos71@gmail.com>
2025-11-06 12:28:41 -0600

    Class Airplane

--- Progra_project/src/main/java/domain/Airplane.java
+++ Progra_project/src/main/java/domain/Airplane.java
@@ -1,29 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package domain;
+
 import java.util.HashMap;
 import java.util.Map;
-/**
- *
- * @author luzma
- */
+
 public class Airplane {
-  private String model;
-  private Map<SeatClass, Integer> capacity;
-  private Map<SeatClass, Integer> booked;
+    private String model;
+    private Map<SeatClass, Integer> capacity;
+    private Map<SeatClass, Integer> booked;
 
-  public enum SeatClass { BUSINESS, ECONOMY }
+    public enum SeatClass { BUSINESS, ECONOMY }
 
-  public Airplane(String model, int execCapacity, int ecoCapacity) {
-      this.model = model;
-      capacity = new HashMap<>();
-      booked = new HashMap<>();
-      capacity.put(SeatClass.BUSINESS, execCapacity);
-      capacity.put(SeatClass.ECONOMY, ecoCapacity);
-      booked.put(SeatClass.BUSINESS, 0);
-      booked.put(SeatClass.ECONOMY, 0);
+    public Airplane(String model, int execCapacity, int ecoCapacity) {
+        this.model = model;
+        capacity = new HashMap<>();
+        booked = new HashMap<>();
+        capacity.put(SeatClass.BUSINESS, execCapacity);
+        capacity.put(SeatClass.ECONOMY, ecoCapacity);
+        booked.put(SeatClass.BUSINESS, 0);
+        booked.put(SeatClass.ECONOMY, 0);
     }
 
     public Map<SeatClass, Integer> getCapacity() {
@@ -43,7 +37,8 @@ public class Airplane {
     }
 
     public String getModel() { 
-        return model; }
+        return model; 
+    }
 
     public boolean hasAvailability(SeatClass seatClass) {
         return booked.get(seatClass) < capacity.get(seatClass);
@@ -66,6 +61,6 @@ public class Airplane {
     @Override
     public String toString() {
         return model + " (Business: " + booked.get(SeatClass.BUSINESS) + "/" + capacity.get(SeatClass.BUSINESS)
-                + " , Ecoomy: " + booked.get(SeatClass.ECONOMY) + "/" + capacity.get(SeatClass.ECONOMY) + ")";
+                + ", Economy: " + booked.get(SeatClass.ECONOMY) + "/" + capacity.get(SeatClass.ECONOMY) + ")";
     }   
 }
